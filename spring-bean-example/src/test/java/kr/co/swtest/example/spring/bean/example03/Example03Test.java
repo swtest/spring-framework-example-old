011dc1bd714f39281e6d1a15f97bf7f2da9efd1d
최영목 <scroogy@swtest.co.kr>
2012-06-13 06:17:53 +0900

    예제3의 패키지명을 no3에서 example03로 수정

--- spring-bean-example/src/test/java/kr/co/swtest/example/spring/bean/example03/Example03Test.java
+++ spring-bean-example/src/test/java/kr/co/swtest/example/spring/bean/example03/Example03Test.java
@@ -1,29 +1,30 @@
-package kr.co.swtest.example.spring.bean;
-
 /*******************************************************************************
  * Copyright(c) 2012 SWTEST. All rights reserved.
  * This software is the proprietary information of SWTEST.
  *******************************************************************************/
+package kr.co.swtest.example.spring.bean.example03;
 
-import kr.co.swtest.example.spring.bean.no3.ServiceFactory;
+import static org.junit.Assert.assertNotNull;
 
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * No3 Main <br/>
+ * Example01Test <br/>
  * 팩토리 메서드를 이용한 빈 생성
  *
  * @author <a href="mailto:scroogy@swtest.co.kr">최영목</a>
- * @since 2012. 6. 12.
+ * @since 2012. 6. 13.
  */
-public class No3Main {
+public class Example03Test {
 
-    public static void main(String[] args) {
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("no3-context.xml");
+    @Test
+    public void test() {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("example03-applicationContext.xml");
 
         ServiceFactory serviceFactory = (ServiceFactory) ctx.getBean("serviceFactory");
-        System.out.println(serviceFactory != null);
+        assertNotNull(serviceFactory);
     }
 
 }

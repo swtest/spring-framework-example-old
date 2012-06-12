0d02aa1a6e1ea316a79f194077ad09038404bd5f
최영목 <scroogy@swtest.co.kr>
2012-06-12 17:15:14 +0900

    빈 프로퍼티에 주입하기 예제 추가

---
+++ spring-bean-example/src/main/java/kr/co/swtest/example/spring/bean/No6_2Main.java
@@ -0,0 +1,29 @@
+package kr.co.swtest.example.spring.bean;
+
+/*******************************************************************************
+ * Copyright(c) 2012 SWTEST. All rights reserved.
+ * This software is the proprietary information of SWTEST.
+ *******************************************************************************/
+
+import kr.co.swtest.example.spring.bean.no6.AndroidAppProgrammer;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * No6_2 Main <br/>
+ * 빈 프로퍼티에 주입하기
+ *
+ * @author <a href="mailto:scroogy@swtest.co.kr">최영목</a>
+ * @since 2012. 6. 12.
+ */
+public class No6_2Main {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("no6-2-context.xml");
+
+        AndroidAppProgrammer androidAppProgrammer = (AndroidAppProgrammer) ctx.getBean("androidAppProgrammer");
+        androidAppProgrammer.work();
+    }
+
+}
---
+++ spring-bean-example/src/main/java/kr/co/swtest/example/spring/bean/no6/AndroidAppProgrammer.java
@@ -0,0 +1,62 @@
+/*******************************************************************************
+ * Copyright(c) 2012 SWTEST. All rights reserved.
+ * This software is the proprietary information of SWTEST.
+ *******************************************************************************/
+package kr.co.swtest.example.spring.bean.no6;
+
+import kr.co.swtest.example.spring.bean.no1.Programmer;
+import kr.co.swtest.example.spring.bean.no2.Computer;
+
+/**
+ * 안드로이드 앱 프로그래머
+ *
+ * @author <a href="mailto:scroogy@swtest.co.kr">최영목</a>
+ * @since 2012. 6. 12.
+ */
+public class AndroidAppProgrammer extends Programmer {
+
+    /** 컴퓨터 */
+    private Computer computer;
+
+    /** 추가 작업시간 */
+    private int addWorkTime;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void work() {
+        super.work();
+        System.out.println(this.addWorkTime + "시간 더 일을 합니다.");
+        System.out.println(this.computer.name() + "으로 일을 합니다.");
+    }
+
+    /**
+     * @return 컴퓨터
+     */
+    public Computer getComputer() {
+        return computer;
+    }
+
+    /**
+     * @param computer 컴퓨터
+     */
+    public void setComputer(Computer computer) {
+        this.computer = computer;
+    }
+
+    /**
+     * @return 추가 작업시간
+     */
+    public int getAddWorkTime() {
+        return addWorkTime;
+    }
+
+    /**
+     * @param addWorkTime 추가 작업시간
+     */
+    public void setAddWorkTime(int addWorkTime) {
+        this.addWorkTime = addWorkTime;
+    }
+
+}
